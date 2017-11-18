38e91fb8036cccdc68b626267fbb7ef6a7fd3240
prakash bhakat <prakash.bhakat2@tcs.com>
2017-11-18 09:59:10 +0530

    Bazzar: Added settinglogic gem
      * Incuded bazzar.yml in .gitignore
      * Added basic settings for the url
      * Deleted swagger_docs file, added mistakenly

---
+++ config/initializers/1_bazzar_settings.rb
@@ -0,0 +1,21 @@
+class BazzarSettings < Settingslogic
+
+  # Load the source file for settings
+  source "#{Rails.root}/config/bazzar.yml"
+  namespace Rails.env
+
+  def self.build_url
+  	custom_port = ":#{port}" unless [443.80].include?(port.to_i)
+  	app_path = 
+  	  [ protocol,
+  	    "://",
+  	    host,
+  	    custom_port,
+  	    relative_url
+  	  ].join('')
+  end
+end
+
+BazzarSettings['host'] ||= 'localhost'
+BazzarSettings['relative_url'] ||= '/'
+BazzarSettings['url'] ||= BazzarSettings.build_url
