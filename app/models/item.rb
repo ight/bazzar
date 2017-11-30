d0704f9d427ccb85bb755f7cf81e9be631cf4c6f
ight <thelight.prakash247@gmail.com>
2017-11-30 15:54:12 +0530

    Bazzar: Added basic table structure and association
      * Added Brand,User,Item,Category and CartItem table
        created a rough associations amoung them.
    
      * Note: This will change as we move on in the project
        and get a bigger picture of the setup

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+
+  # Associations
+  belongs_to :category, inverse_of: :items
+  belongs_to :brand, inverse_of: :items
+  has_many :cart_items, inverse_of: :item
+  has_many :users, through: :cart_items
 end
